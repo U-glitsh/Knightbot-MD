ac6a938715f8d7c28426a382c49bfe35174bde21
U-glitsh <itsmosscow@gmail.com>
2025-01-31 05:39:09 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const settings = {
-  packname: 'Knight',
+  packname: '7rB Alex',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "7rB Alex",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  ownerNumber: '201210307869', //Your number
+  giphyApiKey: 'ql7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
